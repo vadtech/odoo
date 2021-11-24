3c95903f1bce204e87d5cb7e408c84dac9ab39e1
vadtech <93185557+vadtech@users.noreply.github.com>
2021-11-24 14:02:35 +0200

    better fields changes

---
+++ hos_production_app/models/main_prod_app.py
@@ -0,0 +1,57 @@
+from odoo import fields, models
+
+class prod_order_app(models.Model):
+	"""real name of the model"""
+	_name = "prod_order.model"
+	_description = "For Production application"
+	_inherit =["mail.thread","mail.activity.mixin"]
+	_rec_name="customer_ref"
+	_order="id,state"
+
+
+	customer_ref=fields.Many2one('sale.order.line',string="sale_order_lines",tracking=True)
+	main_sales_id=fields.Many2one('sale.order',string="Production_order",tracking=True)
+	orderLines_ids=fields.One2many(related='main_sales_id.order_line', string="")
+
+
+	order=fields.Datetime(related='main_sales_id.date_order' ,string="Order Date")
+	customer_ref= fields.Char(related='main_sales_id.partner_id.name')
+	deli_address=fields.Many2one(related='main_sales_id.partner_invoice_id' ,string="Delivery Address")
+	delivery_date=fields.Datetime(string="Delivery Date",tracking=True)
+	delivery_week=fields.Integer(string="Delivered Week",tracking=True)
+	delivered_date=fields.Date(string="Delivered Date",tracking=True)
+
+	
+
+	delivered_all=fields.Boolean(related='main_sales_id.all_del' ,string="Delivered All Units",default="True")
+	total_vol=fields.Float(string="Total Volume",default="0.00")
+	total_wei=fields.Float(string="Total Weight",default="0.00")
+	total_ite=fields.Char(string="Total iteams")
+
+	state=fields.Selection(
+        string='Status',
+        tracking=True,
+        default='new',
+        selection=[
+        ('new','New'),
+        ('prod','In Production'),
+        ('done','Done'),
+        ('cancel','Cancel'),
+        ('delivered','Delivered')])
+
+
+	def action_new(self):
+		self.state='new'
+
+	def action_prod(self):
+		self.state='prod'
+
+	def action_done(self):
+		self.state='done'
+
+	def action_cancelled(self):
+		self.state='cancel'
+
+
+	def action_delivered(self):
+		self.state='delivered'
