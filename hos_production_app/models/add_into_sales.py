ec18c719694ae88a44d18cf74dab823a881551b8
vadtech <93185557+vadtech@users.noreply.github.com>
2021-11-26 15:23:27 +0200

    Delete hos_production_app directory

--- hos_production_app/models/add_into_sales.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class add_into_sales(models.Model):
-	"""real name of the model"""
-	_inherit ="sale.order"
-	_description = "Moddification of sales table"
-	price_list=fields.Many2one('product.pricelist',string="Price list")
-	
-
