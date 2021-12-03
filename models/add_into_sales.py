f41a32afd7066c0ae2f23abef8d1f941820bc82c
vadtech <93185557+vadtech@users.noreply.github.com>
2021-12-03 06:12:35 +0200

    Update add_into_sales.py

--- models/add_into_sales.py
+++ models/add_into_sales.py
@@ -5,16 +5,6 @@ class add_into_sales(models.Model):
 	_inherit ="sale.order"
 	_description = "Moddification of sales table"
 	price_list=fields.Many2one('product.pricelist',string="Price list")
-	delv_terms=fields.Selection(
-        string='Delivery Terms',
-        default='del_1',
-        selection=[
-        ('del_1','delivery 1'),
-        ('del_2','delivery 2'),
-        ('del_3','delivery 3'),
-        ('del_4','delivery 4')])
-	total_dis=fields.Integer( string='Total Discount')
-	total_dis_line=fields.Integer(string='Total line')
-	all_del=fields.Boolean(string="All Iteams as Delivered?",default="False")
+
 	
 
