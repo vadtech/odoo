a13cb05467f3bfab95ff28fbf4615090a2f1fe9b
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-17 13:50:23 +0200

    Update add_into_acc_line.py

--- production_app/models/add_into_acc_line.py
+++ production_app/models/add_into_acc_line.py
@@ -5,14 +5,7 @@ class add_into_order_line(models.Model):
 	_inherit ="account.move.line"
 	_description="Moddification of aacount move line"
 
-	acc_lineDiscount=fields.Integer( string='Line Discount %')
-	acc_disAmount=fields.Integer( string='line Amount' )
-
-	@api.onchange("acc_lineDiscount")
-	def _onchange_lineDiscount(self):
-		self.acc_disAmount = self.acc_lineDiscount/100 * self.price_subtotal
-		temp=self.price_subtotal
-		self.price_subtotal=temp-self.acc_disAmount
+	acc_disAmount=fields.Integer( string='line Discount Amount')
 
 
 
