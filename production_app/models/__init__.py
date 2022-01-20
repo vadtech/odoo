527a2b2f4356026545f600f5c20051002b6bece9
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-20 21:06:13 +0200

    Delete production_app directory

--- production_app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import main_prod_app
-from . import add_into_sales
-from . import inherit_sales_order
-from . import add_into_res
-from . import add_into_acc_move
-from . import add_into_acc_line
-#from . import logs_report
-
- 
