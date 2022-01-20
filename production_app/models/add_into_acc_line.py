b7b29a03e808f170b6cdd05f0d8ba7aae45a9b2a
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-20 22:40:34 +0200

    Add files via upload

---
+++ production_app/models/add_into_acc_line.py
@@ -0,0 +1,13 @@
+from odoo import fields, models, api ,_
+
+class add_into_order_line(models.Model):
+	"""real name of the model"""
+	_inherit ="account.move.line"
+	_description="Moddification of aacount move line"
+
+	acc_disAmount=fields.Integer( string='line Discount Amount')
+
+
+
+ 
+
