34c50b8ebef07e6cfc20acd9b6cb0cf4a849111e
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-21 10:44:41 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -21,7 +21,7 @@ class prod_order_app(models.Model):
 	customer_ref= fields.Char(related='main_sales_id.partner_id.name')
 	deli_address=fields.Many2one(related='main_sales_id.partner_invoice_id' ,string="Delivery Address")
 	delivery_date=fields.Datetime(related='main_sales_id.expected_date', string="Delivery Date",tracking=True)
-	delivery_week=fields.Integer(string="Delivered Week",tracking=True)
+	delivery_week=fields.Integer(compute="_del_week",string="Delivered Week",tracking=True)
 	delivered_date=fields.Date(string="Delivered Date",tracking=True)
 
 	all_del = fields.Boolean(string="All iteams as Delivered?", default=False)
@@ -38,6 +38,13 @@ class prod_order_app(models.Model):
         ('prod','In Production'),
         ('cancel','Cancel'),
         ('delivered','Delivered')])
+	
+	def _del_week(self):
+		for rec in self:
+			if rec.delivery_date==False:
+				pass
+			else:
+				rec.delivery_week=rec.delivery_date.strftime("%w")
 	
 	@api.onchange("all_del")
 	def _onchange_alldel(self):
