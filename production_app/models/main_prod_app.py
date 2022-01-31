3da4cdfaa9f2a224a3235f63c5e2c62083799949
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-31 04:14:44 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -26,7 +26,7 @@ class prod_order_app(models.Model):
 	
 	delivery_date=fields.Datetime(related='main_sales_id.expected_date', string="Delivery Date",tracking=True)
 	delivery_week=fields.Integer(string="Delivered Week",tracking=True)
-	delivered_date=fields.Date(string="Delivered Date",tracking=True)
+	delivered_date=fields.Date(compute="_del_week",string="Delivered Date",tracking=True)
 
 	all_del = fields.Boolean(string="All iteams as Delivered?", default=False)
 	total_vol=fields.Float(string="Total Volume(dm3)",default="0.00")
@@ -43,12 +43,12 @@ class prod_order_app(models.Model):
         ('cancel','Cancel'),
         ('delivered','Delivered')])
 	
-# 	def _del_week(self):
-# 		for rec in self:
-# 			if rec.delivery_date==False:
-# 				pass
-# 			else:
-# 				rec.delivery_week=rec.delivery_date.strftime("%w")
+	def _del_week(self):
+		for rec in self:
+			if rec.delivery_date==False:
+				pass
+			else:
+				rec.delivery_week=rec.delivery_date.strftime("%w")
 	
 	@api.onchange("all_del")
 	def _onchange_alldel(self):
