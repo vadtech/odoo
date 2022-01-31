501a7bdb390ae7548a21dc46a9e2a7cea48a49aa
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-01-31 04:21:11 +0200

    Update add_into_acc_move.py

--- production_app/models/add_into_acc_move.py
+++ production_app/models/add_into_acc_move.py
@@ -8,7 +8,7 @@ class add_into_acc(models.Model):
 	link_acc_id = fields.Many2one('account.move', string="Inovince ID")
 	isPrinted = fields.Boolean(string="IS Printed", default=False)
 	brch_no=fields.Integer(string="branch No")
-	sale_order = fields.Char(related='link_acc_id.sales_char')
+	sale_order = fields.Char(related='link_acc_id.sales_id_char')
 	custmer = fields.Char(related='link_acc_id.invoice_partner_display_name')
 	inv_no= fields.Char(related='link_acc_id.invoice_no_name')
 
