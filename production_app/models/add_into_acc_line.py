ae58d30f509e6eb8fdf15199818245cbd216e453
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-02-18 13:40:24 +0200

    Update add_into_acc_line.py

--- production_app/models/add_into_acc_line.py
+++ production_app/models/add_into_acc_line.py
@@ -5,8 +5,8 @@ class add_into_order_line(models.Model):
 	_inherit ="account.move.line"
 	_description="Moddification of aacount move line"
 
-	acc_disAmount=fields.Integer( string='line Discount Amount' compute="move_id.link_prod_id.main_sales_id.acc_disAmount")
-	linediscPerct=fields.Integer( string='line Discount %' compute="move_id.link_prod_id.main_sales_id.linediscPerct")
+	acc_disAmount=fields.Integer( string='line Discount Amount', compute="move_id.link_prod_id.main_sales_id.acc_disAmount")
+	linediscPerct=fields.Integer( string='line Discount %', compute="move_id.link_prod_id.main_sales_id.linediscPerct")
 
 
 
