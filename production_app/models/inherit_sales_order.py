3ee40aa09fe3a8d3c868910f8ecabea327d3f648
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-03-07 00:55:58 +0200

    Update inherit_sales_order.py

--- production_app/models/inherit_sales_order.py
+++ production_app/models/inherit_sales_order.py
@@ -14,5 +14,14 @@ class add_into_order_line(models.Model):
 
 	def _cal_disamount(self):
 		for rec in self:
-			rec.disAmount = rec.linediscPerct/100 * rec.price_unit *  rec.product_uom_qty
-			rec.price_subtotal = rec.price_subtotal - rec.disAmount
+			rec.disAmount = rec.linediscPerct / 100 * rec.price_unit * rec.product_uom_qty
+			dismount = rec.discount / 100 * rec.price_unit * rec.product_uom_qty
+			rec.price_subtotal= rec.price_unit * rec.product_uom_qty - rec.disAmount - dismount
+			amount_untaxed = amount_tax = 0.0
+			for line in self.order_id.order_line:
+				amount_untaxed += line.price_subtotal
+				amount_tax += line.tax_id.amount / 100 * line.price_subtotal
+	
+			self.order_id.amount_untaxed = amount_untaxed
+			self.order_id.amount_tax = amount_tax
+			self.order_id.amount_total = amount_untaxed + amount_tax
