55b49058061c54a7a8e055f3e3d47d2df19042b8
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-03-25 11:49:06 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -203,6 +203,7 @@ class prod_order_app(models.Model):
 						'untaxed_amt': record_to_update.amount_untaxed,
 						'mva': record_to_update.amount_tax,
 						'total': record_to_update.amount_total,
+						'dte_create': record_to_update.invoice_date
 					})
 
 class log_invoice_app(models.Model):
@@ -217,6 +218,8 @@ class log_invoice_app(models.Model):
 	untaxed_amt = fields.Integer(string="Untaxed Amt")
 	mva = fields.Integer(string="mva")
 	total = fields.Integer(string="total")
+	dte_create=fields.Datetime(string="date Create")
+	
 
 	log_state = fields.Selection(
 		string='log_state',
