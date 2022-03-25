752696c4b73dd27beaa528d9a5684b0fcb6a20e2
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-03-25 12:33:02 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -195,7 +195,7 @@ class prod_order_app(models.Model):
 					}
 					record_to_update.write(vali)
 					self.env['logs.model'].create({
-						'acc_move_id': record_to_update.id,
+						'acc_move_id': record_to_update.invoice_no_name,
 						'log_state': 'create',
 						'inv_date': record_to_update.invoice_date,
 						'due_date': record_to_update.invoice_date_due,
