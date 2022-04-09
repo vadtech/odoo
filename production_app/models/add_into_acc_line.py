c654cc7b9aa1544eac0f781071185d68cd7e77fc
Estain Makaudze <56034279+Estain-Makaudze@users.noreply.github.com>
2022-04-09 18:16:49 +0200

    Update add_into_acc_line.py

--- production_app/models/add_into_acc_line.py
+++ production_app/models/add_into_acc_line.py
@@ -10,27 +10,14 @@ class add_into_order_line(models.Model):
 
 	def _cal_disamount(self):
 		list_id=[27665,46,45,44,43]
-		if self.move_id.id in list_id:
-			for rec in self:
-				#calculate line discount amount
-				rec.acc_disAmount = rec.linediscPerct / 100 * rec.price_unit * rec.quantity
-				#calculate discount amount
-				dismount = rec.discount / 100 * rec.price_unit * rec.quantity
-				#calculate discount price subtotal
-				rec.price_subtotal= dismount
-				amount_untaxed = amount_tax = 0.0
-				for line in self.move_id.invoice_line_ids:
-					amount_untaxed += line.price_subtotal
-					amount_tax += line.tax_ids.amount / 100 * line.price_subtotal
-				self.move_id.\
-					amount_untaxed = amount_untaxed
-				self.move_id.amount_tax = amount_tax
-				self.move_id.amount_total = amount_untaxed + amount_tax
-		else:
+		def _cal_disamount(self): 
 			for rec in self:
 				rec.acc_disAmount = rec.linediscPerct / 100 * rec.price_unit * rec.quantity
 				dismount = rec.discount / 100 * rec.price_unit * rec.quantity
-				rec.price_subtotal= dismount
+				if self.move_id.id in list_id and dismount != 0:
+					rec.price_subtotal=dismount
+				else:
+					rec.price_subtotal= rec.price_unit * rec.quantity - rec.acc_disAmount - dismount
 				amount_untaxed = amount_tax = 0.0
 				for line in self.move_id.invoice_line_ids:
 					amount_untaxed += line.price_subtotal
