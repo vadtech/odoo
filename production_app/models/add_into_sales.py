6a37673c421a87f5692c744022c2a4d16de52392
innocent.mpasi@inonit.no <56034279+innocent-mpasi@users.noreply.github.com>
2022-04-25 12:35:57 +0200

    Update add_into_sales.py

--- production_app/models/add_into_sales.py
+++ production_app/models/add_into_sales.py
@@ -211,7 +211,10 @@ class add_into_sales(models.Model):
 							# calculte to be subtotal
 							to_be_subtotal = lin.price_total - recs.price_total
 							# calacultor discount
-							new_discount = (100 * to_be_subtotal - 100 * lin.price_unit * lin.quantity) / -lin.price_unit * lin.quantity * 1
+							if lin.price_unit == 0 or lin.quantity ==0: 
+								new_discount = 0
+							else:
+								new_discount = (100 * to_be_subtotal - 100 * lin.price_unit * lin.quantity) / -lin.price_unit * lin.quantity * 1
 							print(lin.name)
 							new_rec.discount=new_discount
 							print(to_be_subtotal)
@@ -227,7 +230,10 @@ class add_into_sales(models.Model):
 							# calculte to be subtotal
 							to_be_subtotal = lin.price_total - recs.price_total
 							# calacultor discount
-							new_discount = (100 * to_be_subtotal - 100 * lin.price_unit * lin.quantity) / -lin.price_unit * lin.quantity * 1
+							if lin.price_unit == 0 or lin.quantity ==0: 
+								new_discount = 0
+							else:
+								new_discount = (100 * to_be_subtotal - 100 * lin.price_unit * lin.quantity) / -lin.price_unit * lin.quantity * 1
 							print(lin.name)
 							new_rec.discount = new_discount
 							print(to_be_subtotal)
