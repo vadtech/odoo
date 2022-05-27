9b956a0a9a96711fb3c118b79f782f057399ca58
vadtech <93185557+vadtech@users.noreply.github.com>
2022-05-27 11:38:24 +0200

    Update inherit_sales_order.py

--- production_app/models/inherit_sales_order.py
+++ production_app/models/inherit_sales_order.py
@@ -19,26 +19,36 @@ class add_into_order_line(models.Model):
 		date_create = datetime.strptime('2022-05-26 0:0:0', '%Y-%m-%d %H:%M:%S')
 		for rec in self:
 			if rec.state=='sale' or rec.state=='done' and rec.create_date>=date_create:
-				rec.disAmount = rec.linediscPerct / 100 * rec.price_unit * rec.product_uom_qty
-				dismount = rec.discount / 100 * rec.price_unit * rec.product_uom_qty
-				rec.price_subtotal= (rec.price_unit * rec.product_uom_qty )- rec.disAmount - dismount
+				# calculate discount
+				disc_ammount = rec.discount / 100 * rec.price_unit * rec.product_uom_qty
+				# calculate temp subtotal
+				temp_price_subtotal = (rec.price_unit * rec.product_uom_qty) - disc_ammount
+				# calculate line discounts and line discount amount
+				rec.disAmount = rec.linediscPerct / 100 * temp_price_subtotal
+				#subtract line discount from subtotal
+				rec.price_subtotal = temp_price_subtotal - rec.disAmount
 				amount_untaxed = amount_tax = 0.0
 				for line in self.order_id.order_line:
 					amount_untaxed += line.price_subtotal
-					amount_tax += line.tax_id.amount/ 100 * line.price_subtotal
+					amount_tax += line.tax_id.amount / 100 * line.price_subtotal
 
 				self.order_id.amount_untaxed = amount_untaxed
 				self.order_id.amount_tax = amount_tax
 				self.order_id.amount_total = amount_untaxed + amount_tax
 				
 			elif rec.create_date>=date_create:
-				rec.disAmount = rec.linediscPerct / 100 * rec.price_unit * rec.product_uom_qty
-				dismount = rec.discount / 100 * rec.price_unit * rec.product_uom_qty
-				rec.price_subtotal= (rec.price_unit * rec.product_uom_qty )- rec.disAmount - dismount
+				# calculate discount
+				disc_ammount = rec.discount / 100 * rec.price_unit * rec.product_uom_qty
+				# calculate temp subtotal
+				temp_price_subtotal = (rec.price_unit * rec.product_uom_qty) - disc_ammount
+				# calculate line discounts and line discount amount
+				rec.disAmount = rec.linediscPerct / 100 * temp_price_subtotal
+				#subtract line discount from subtotal
+				rec.price_subtotal = temp_price_subtotal - rec.disAmount
 				amount_untaxed = amount_tax = 0.0
 				for line in self.order_id.order_line:
 					amount_untaxed += line.price_subtotal
-					amount_tax += line.tax_id.amount/ 100 * line.price_subtotal
+					amount_tax += line.tax_id.amount / 100 * line.price_subtotal
 
 				self.order_id.amount_untaxed = amount_untaxed
 				self.order_id.amount_tax = amount_tax
