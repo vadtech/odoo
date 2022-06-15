e69e97c92d8936cb3c22fa46fd1d0471e9628f17
innocent-mpasi <56034279+innocent-mpasi@users.noreply.github.com>
2022-06-15 09:49:52 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 class prod_order_app(models.Model):
 	"""real name of the model"""  
 	_name = "prod_order.model"
-	_description = "For Production application"
+	_description = "For Production application"  
 	_inherit =["mail.thread","mail.activity.mixin"]
 	_rec_name="customer_ref"
 	_order="id desc"
