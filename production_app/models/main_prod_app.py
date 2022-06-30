8ea46c7d0265f0af1844a38fc71f8ecef21f97a2
innocent-mpasi <56034279+innocent-mpasi@users.noreply.github.com>
2022-06-30 12:50:47 +0200

    Update main_prod_app.py

--- production_app/models/main_prod_app.py
+++ production_app/models/main_prod_app.py
@@ -33,7 +33,7 @@ class prod_order_app(models.Model):
 	delivery_wee=fields.Integer(string="Delivery Week")
 
 
-	all_del = fields.Boolean(string="All iteams as Delivered?", default=False)
+	all_del = fields.Boolean(string="All items as finished?", default=False)
 	total_vol=fields.Float(string="Total Volume(dm3)",default="0.00")
 	total_wei=fields.Float(string="Total Weight(kg)",default="0.00")
 	total_ite=fields.Char(string="Total Iteams")
