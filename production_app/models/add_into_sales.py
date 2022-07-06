846b9fa3c9b31dda035ba985af36a2a2ab2ae9ec
innocent-mpasi <56034279+innocent-mpasi@users.noreply.github.com>
2022-07-06 15:03:39 +0200

    Update add_into_sales.py

--- production_app/models/add_into_sales.py
+++ production_app/models/add_into_sales.py
@@ -11,7 +11,7 @@ class add_into_sales(models.Model):
     _inherit = "sale.order"
     _description = "Moddification of sales table"
     
-    partner_id = fields.Many2one('res.partner', string='Customer' domain="[('category_id', '=', 'Main Customer')]") 
+    partner_id = fields.Many2one('res.partner', string='Customer' ,domain="[('category_id', '=', 'Main Customer')]") 
     price_list = fields.Many2one('product.pricelist', string="Price list")
     total_dis = fields.Integer(string='Total Discount')
     total_dis_line = fields.Integer(string='Total line')
